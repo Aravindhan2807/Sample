ad0f38526011b4e1737ef1f22e9b0fb99465a8f1
Aravindhan2807 <101921706+Aravindhan2807@users.noreply.github.com>
2022-12-31 11:25:16 +0530

    updated file

--- src/main/java/com/sample/Application/SampleApplication.java
+++ src/main/java/com/sample/Application/SampleApplication.java
@@ -1,4 +1,4 @@
-package com.sample.Sample;
+package com.sample.Application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/sample/Application/SampleApplicationTests.java
+++ src/test/java/com/sample/Application/SampleApplicationTests.java
@@ -1,4 +1,4 @@
-package com.sample.Sample;
+package com.sample.Application;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
